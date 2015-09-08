7d5290623bdea6a1834130719020ad29427d7650
Charles <whitfield.cw@gmail.com>
2015-09-07 18:21:55 -0700

    add devise and create user

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,7 @@ RSpec.configure do |config|
   # config.include Devise::TestHelpers, :type => :controller
 
   # config.include Features, :type => :feature
-  # config.include Features::SessionHelpers, type: :feature
+  config.include Features::SessionHelpers, type: :feature
 
   # config.include Warden::Test::Helpers
   # config.before :suite do
