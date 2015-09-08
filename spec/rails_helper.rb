3bff82f4f87612eff6220575513f545aa939c243
Charles <whitfield.cw@gmail.com>
2015-09-07 18:27:58 -0700

    passing feature test for devise login

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,10 +32,10 @@ RSpec.configure do |config|
   # config.include Features, :type => :feature
   config.include Features::SessionHelpers, type: :feature
 
-  # config.include Warden::Test::Helpers
-  # config.before :suite do
-  #   Warden.test_mode!
-  # end
+  config.include Warden::Test::Helpers
+  config.before :suite do
+    Warden.test_mode!
+  end
 
   # config.include Paperclip::Shoulda::Matchers
 
